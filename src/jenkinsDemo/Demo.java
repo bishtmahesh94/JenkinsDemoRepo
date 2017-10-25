1857e2dcde54b283f0e5c109d7a39fe4cf22675c
Mahesh <bishtmahesh94@gmail.com>
2017-10-25 00:46:57 -0700

    commit

--- src/jenkinsDemo/Demo.java
+++ src/jenkinsDemo/Demo.java
@@ -1,22 +1,39 @@
 package jenkinsDemo;
 
+import org.apache.log4j.xml.DOMConfigurator;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
-
+import org.apache.log4j.Logger;
 public class Demo {
 	
 	private WebDriver driver; 
-	
+	static Logger log = Logger.getLogger(Demo.class.getName());
 	@Test
-	public void main(){
-		
-	System.out.println("hello");
+	public void main() throws Exception{
+		 DOMConfigurator.configure("log4j.xml");
 	
-	driver=new FirefoxDriver();
-	driver.get("http://www.google.com");
+		 log.info("hello LOGGER");
+		 System.out.println("hello");
+	
+	 driver=new FirefoxDriver();
+	 log.info("Browser Launch");
+	 
+	 driver.get("http://www.google.com");
+	 log.info("url access");
 	System.out.println(driver.getTitle());
-	driver.quit();
+	  try 
+	    {
+	        driver.close();
+	        log.info("browser closed");
+	        Runtime.getRuntime().exec("taskkill /F /IM IEDriverServer.exe");
+
+	    }
+	    catch (Exception anException) 
+	    {
+	    	 log.info("Exception");
+	        anException.printStackTrace();
+	    }
 			}
 	}
 
